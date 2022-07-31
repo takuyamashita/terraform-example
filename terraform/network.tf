e5836c9e4ad936e4aab51e0ce8c4f22f90400cee
takuyamashita <57676798+takuyamashita@users.noreply.github.com>
2022-07-31 18:51:20 +0900

    add route table

--- terraform/network.tf
+++ terraform/network.tf
@@ -13,9 +13,9 @@ resource "aws_vpc" "vpc" {
 }
 
 resource "aws_subnet" "public_subnet_1a" {
-  vpc_id                            = aws_vpc.vpc.id
-  availability_zone                 = "ap-northeast-1a"
-  cidr_block                        = "192.168.1.0/24"
+  vpc_id                  = aws_vpc.vpc.id
+  availability_zone       = "ap-northeast-1a"
+  cidr_block              = "192.168.1.0/24"
   map_public_ip_on_launch = true
 
   tags = {
@@ -27,9 +27,9 @@ resource "aws_subnet" "public_subnet_1a" {
 }
 
 resource "aws_subnet" "public_subnet_1c" {
-  vpc_id                            = aws_vpc.vpc.id
-  availability_zone                 = "ap-northeast-1c"
-  cidr_block                        = "192.168.2.0/24"
+  vpc_id                  = aws_vpc.vpc.id
+  availability_zone       = "ap-northeast-1c"
+  cidr_block              = "192.168.2.0/24"
   map_public_ip_on_launch = true
 
   tags = {
@@ -41,9 +41,9 @@ resource "aws_subnet" "public_subnet_1c" {
 }
 
 resource "aws_subnet" "private_subnet_1a" {
-  vpc_id                            = aws_vpc.vpc.id
-  availability_zone                 = "ap-northeast-1a"
-  cidr_block                        = "192.168.3.0/24"
+  vpc_id                  = aws_vpc.vpc.id
+  availability_zone       = "ap-northeast-1a"
+  cidr_block              = "192.168.3.0/24"
   map_public_ip_on_launch = false
 
   tags = {
@@ -55,9 +55,9 @@ resource "aws_subnet" "private_subnet_1a" {
 }
 
 resource "aws_subnet" "private_subnet_1c" {
-  vpc_id                            = aws_vpc.vpc.id
-  availability_zone                 = "ap-northeast-1c"
-  cidr_block                        = "192.168.4.0/24"
+  vpc_id                  = aws_vpc.vpc.id
+  availability_zone       = "ap-northeast-1c"
+  cidr_block              = "192.168.4.0/24"
   map_public_ip_on_launch = false
 
   tags = {
@@ -66,4 +66,46 @@ resource "aws_subnet" "private_subnet_1c" {
     Env     = var.environment
     Type    = "private"
   }
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-public-rt"
+    Project = var.project
+    Env     = var.environment
+    Type    = "public"
+  }
+}
+
+resource "aws_route_table_association" "public_rt_1a" {
+  route_table_id = aws_route_table.public_rt.id
+  subnet_id      = aws_subnet.public_subnet_1a.id
+}
+
+resource "aws_route_table_association" "public_rt_1c" {
+  route_table_id = aws_route_table.public_rt.id
+  subnet_id      = aws_subnet.public_subnet_1c.id
+}
+
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-private-rt"
+    Project = var.project
+    Env     = var.environment
+    Type    = "private"
+  }
+}
+
+resource "aws_route_table_association" "private_rt_1a" {
+  route_table_id = aws_route_table.private_rt.id
+  subnet_id      = aws_subnet.private_subnet_1a.id
+}
+
+resource "aws_route_table_association" "private_rt_1c" {
+  route_table_id = aws_route_table.private_rt.id
+  subnet_id      = aws_subnet.private_subnet_1c.id
 }
